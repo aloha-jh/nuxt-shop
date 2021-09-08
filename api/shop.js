766c5f4b385b427257864fd0aa9a05ca01fdfb30
alohajh2@gmail.com <alohajh2@gmail.com>
2021-09-08 11:08:34 +0900

    create

---
+++ api/shop.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+})
+
+function fetchProducts() {
+  return instance.get(`/products`)
+}
+
+function fetchProductById(id) {
+  return instance.get(`/products/${id}`)
+}
+
+function fetchProductsByKeyword(keyword) {
+  return instance.get(`/products`, {
+    params: {
+      name_like: keyword,
+    },
+  })
+}
+
+function fetchCartItems() {
+  return instance.get('/carts')
+}
+
+function createCartItem(cartItem) {
+  return instance.post('/carts', cartItem)
+}
+
+export {
+  fetchProducts,
+  fetchProductById,
+  fetchProductsByKeyword,
+  createCartItem,
+  fetchCartItems,
+}
